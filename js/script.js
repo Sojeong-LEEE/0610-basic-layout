6560d2708e3ddb639949150df945f0336731732d
sojeong <lsojxx@gmail.com>
2021-06-10 14:12:01 +0900

    0610-basic-layout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* 기본 세팅 */
+$(function(){
+    /* header scroll event*/
+
+    var $toggle = $('.toggle'),
+    $gnb = $('.gnb');
+
+    $($toggle).click(function(){
+        $(this).toggleClass('active');
+        $($gnb).toggleClass('visible');
+    })
+    /* gnb visible */
+
+    /* dimmed effect*/
+
+    /* gnb__item click */
+
+    /* arrow up scroll */
+
+    
+});
